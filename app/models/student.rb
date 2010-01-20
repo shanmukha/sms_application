45d41f4f986906a25521f0a6006c1887c12b6783
shanmukha <shanmukhakatta@gmail.com>
2010-01-20 12:57:33 +0500

    latest changes

--- app/models/student.rb
+++ app/models/student.rb
@@ -30,12 +30,11 @@ class Student < ActiveRecord::Base
       students = Group.find(group_id).students
       names = []
       sizes = []
-      students.each do |student|
-      	names << student.name
       	conditions << ["group_id = ?",group_id] 
         conditions << ['message_students.status =?','Delivered'] if type=="messages"
-      	conditions << ['message_students.student_id = ?',student.id]  if type=="messages"
-      	students_communication_size[student.id]  = Message.count(:all,:joins =>[:message_students],:conditions => [ conditions.transpose.first.join( " and " ), *conditions.transpose.last ] ) if type=="messages"
+      students.each do |student|
+       names << student.name
+       students_communication_size[student.id]  = student.messages.count(:all,:conditions => [ conditions.transpose.first.join( " and " ), *conditions.transpose.last ] ) if type=="messages"
       	students_communication_size[student.id] = student.letters.count(:all,:conditions => [ conditions.transpose.first.join( " and " ), *conditions.transpose.last ] ) if type=="letters" 
       	students_communication_size[student.id] = student.emails.find(:all,:conditions => [ conditions.transpose.first.join( " and " ), *conditions.transpose.last ] ).size  if type=="emails" 
       	sizes << students_communication_size[student.id]
