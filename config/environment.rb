6fcb98ef43c49390497b53eab537ac6f3f590fb9
unknown <User1@.(none)>
2010-01-30 08:57:21 +0530

    style sheets

--- config/environment.rb
+++ config/environment.rb
@@ -32,8 +32,8 @@ require File.join(File.dirname(__FILE__), 'boot')
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
-   config.gem "binarylogic-authlogic",:lib =>"authlogic.rb",:version =>"2.1.1"
-   config.gem "mattetti-googlecharts",:lib => "gchart.rb",:version => "1.4.0"
+   config.gem "authlogic",:lib =>"authlogic.rb",:version =>"2.1.1"
+   config.gem "googlecharts",:lib => "gchart.rb",:version => "1.4.0"
    config.gem "searchlogic"
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
