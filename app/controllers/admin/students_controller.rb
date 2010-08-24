46f46461df29bb114e21cfb8147977b1f1a1b55a
Isiri <isiri@isiri-desktop.(none)>
2010-08-24 09:18:33 +0530

    latest changes

--- app/controllers/admin/students_controller.rb
+++ app/controllers/admin/students_controller.rb
@@ -32,7 +32,8 @@ class Admin::StudentsController < ApplicationController
     from_date = "#{academic_year.from_date} 00:00:00"
     to_date = "#{academic_year.to_date} 23:59:59"   
     @marks = @student.marks.find(:all,:conditions => ['created_at >= ? and created_at <= ?',from_date,to_date]) 
-    respond_to do |format|
+    @exam_names,@percentages = Exam.find_exam_names_and_percentage(@student,academic_year,school)
+   respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @student }
     end
--- app/models/exam.rb
+++ app/models/exam.rb
@@ -8,11 +8,25 @@ class Exam < ActiveRecord::Base
  has_many :subjects, :through => :exam_subjects
  validates_presence_of :exam_type
   
- def self.find_exam_names_and_percentage(academic_year,school)
+ def self.find_exam_names_and_percentage(student,academic_year,school)
     percentages =[]
     exam_names =[] 
-    Exam.find(:all,:include=>[:exam_classes],:conditions => ['exam_classes.academic_year_id =? and school_id =?',academic_year.id,school.id]).map{|object|exam_names << object.name} 
-   return exam_names 
+    
+    exam_names = Exam.find(:all,:include=>[:exam_classes,:marks],:conditions => ['exam_classes.academic_year_id =? and school_id =? and marks.student_id =?',academic_year.id,school.id,student.id]).map{|object| object.name}.join('|')
+   exams =  Exam.find(:all,:include=>[:exam_classes,:marks],:conditions => ['exam_classes.academic_year_id =? and school_id =? and marks.student_id =?',academic_year.id,school.id,student.id])
+   subjects = Subject.find(:all,:conditions =>['school_id =?',school.id])
+   exams.each do |exam|
+    total_maximum_marks =0
+    total_marks =0
+   subjects.each do |subject|
+   maximum_marks = ExamSubject.find(:first,:conditions =>['exam_id =? and subject_id =?',exam.id,subject.id]).maximum_marks rescue 0
+    total_maximum_marks+= maximum_marks
+    mark= Mark.find(:first,:conditions =>['exam_id =? and subject_id =? and student_id =?',exam.id,subject.id,student.id]).mark rescue 0
+    total_marks+= mark
+    end
+   percentages<<  (total_maximum_marks/100).to_f*total_marks
+  end
+  return exam_names,percentages
 end
 end
 
