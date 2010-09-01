4b5a539cb726cab42b4fe4930ac0e5b558526be6
Isiri <isiri@isiri-desktop.(none)>
2010-09-01 10:33:55 +0530

    added latest changes

--- app/helpers/marks_helper.rb
+++ app/helpers/marks_helper.rb
@@ -6,4 +6,10 @@ def find_mark(group,exam,subject,student)
  mark = Mark.find(:first,:conditions=>['exam_id =? and group_id =? and subject_id =? and student_id =?',exam.id,group.id,subject.id,student.id]).mark rescue ""
  return mark
 end
+ def find_exam_subjects(exam)
+  exam.subjects.find(:all,:group =>:name)
+ end
+ def find_group_studnets(group)
+  group.students.find(:all,:group =>:name,:conditions => ['status =?','Active'])
+ end
 end
